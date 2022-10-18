0ce8656f8c846b90ef52996ce8eade97183c5d0f
Kujtim.Prenku <Kujtim.Prenku@sqa-consulting.com>
2022-10-18 22:57:44 +0200

    Setup wallet selector, modal-ui and the ledger module.

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,42 @@
 <script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import { onMounted, shallowRef } from "vue";
+
+import { setupWalletSelector, WalletSelector } from "@near-wallet-selector/core";
+import {setupModal, WalletSelectorModal} from "@near-wallet-selector/modal-ui";
+import { setupLedger } from "@near-wallet-selector/ledger";
+
+import { CONTRACT_ID } from "./constants";
+
+declare global {
+  interface Window {
+    selector: WalletSelector;
+    modal: WalletSelectorModal;
+  }
+}
+
+const selector = shallowRef<WalletSelector>();
+const modal = shallowRef<WalletSelectorModal>();
+
+onMounted(async () => {
+  const _selector = await setupWalletSelector({
+    network: "testnet",
+    modules: [setupLedger()],
+  });
+
+  const _modal = setupModal(_selector,{ contractId: "CONTRACT_ID" });
+  selector.value = _selector;
+  modal.value = _modal;
+
+  window.selector = _selector;
+  window.modal = _modal;
+});
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <h1>NEAR Guest Book</h1>
+  <button v-if="modal" @click="modal.show()">Login</button>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+<style>
+
 </style>
