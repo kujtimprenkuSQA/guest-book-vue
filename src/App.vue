5f72109b9b9236b7d643d4eca26315fd2b37a464
Kujtim.Prenku <Kujtim.Prenku@sqa-consulting.com>
2022-10-19 20:40:13 +0200

    Added support for all wallets.

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,17 @@ import Content from "./components/Content.vue";
 
 import { setupWalletSelector } from "@near-wallet-selector/core";
 import {setupModal } from "@near-wallet-selector/modal-ui";
-import { setupLedger } from "@near-wallet-selector/ledger";
-import { setupMyNearWallet } from "@near-wallet-selector/my-near-wallet";
+import { setupDefaultWallets } from "@near-wallet-selector/default-wallets";
+import { setupNearWallet } from "@near-wallet-selector/near-wallet";
+import { setupSender } from "@near-wallet-selector/sender";
+import { setupNearFi } from "@near-wallet-selector/nearfi";
 import { setupMathWallet } from "@near-wallet-selector/math-wallet";
+import { setupNightly } from "@near-wallet-selector/nightly";
+import { setupMeteorWallet } from "@near-wallet-selector/meteor-wallet";
+import { setupNightlyConnect } from "@near-wallet-selector/nightly-connect";
+import { setupWalletConnect } from "@near-wallet-selector/wallet-connect";
+import { setupCoin98Wallet } from "@near-wallet-selector/coin98-wallet";
+import { setupHereWallet } from "@near-wallet-selector/here-wallet";
 
 import type { WalletSelector, AccountState } from "@near-wallet-selector/core";
 import type { WalletSelectorModal } from "@near-wallet-selector/modal-ui";
@@ -22,9 +30,33 @@ onMounted(async () => {
   const _selector = await setupWalletSelector({
     network: "testnet",
     modules: [
-      setupMyNearWallet(),
-      setupLedger(),
+      ...(await setupDefaultWallets()),
+      setupNearWallet(),
+      setupSender(),
       setupMathWallet(),
+      setupNightly(),
+      setupMeteorWallet(),
+      setupHereWallet(),
+      setupCoin98Wallet(),
+      setupNearFi(),
+      setupWalletConnect({
+        projectId: "c4f79cc...",
+        metadata: {
+          name: "NEAR Wallet Selector",
+          description: "Example dApp used by NEAR Wallet Selector",
+          url: "https://github.com/near/wallet-selector",
+          icons: ["https://avatars.githubusercontent.com/u/37784886"],
+        },
+      }),
+      setupNightlyConnect({
+        url: "wss://relay.nightly.app/app",
+        appMetadata: {
+          additionalInfo: "",
+          application: "NEAR Wallet Selector",
+          description: "Example dApp used by NEAR Wallet Selector",
+          icon: "https://near.org/wp-content/uploads/2020/09/cropped-favicon-192x192.png",
+        },
+      }),
     ],
   });
 
