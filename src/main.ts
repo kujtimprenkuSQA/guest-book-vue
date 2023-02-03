c8f3683c8ce09c39b06bceedb058e428ca71e1fc
Kujtim Prenku <kujtim.prenku@sqa-consulting.com>
2023-02-03 12:37:15 +0100

    Revert "Add show acccount-export modal."
    This reverts commit e32bf2f2df0fd7d0006277b822567aedeff0045b.

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { createApp } from "vue";
 import "./shims";
 import "./style.css";
 import "@near-wallet-selector/modal-ui/styles.css";
-import "@near-wallet-selector/account-export/styles.css";
 import App from "./App.vue";
 
 createApp(App).mount("#app");
